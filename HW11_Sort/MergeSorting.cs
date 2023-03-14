41dc27bcde144ae0bf8c908a0c2d32c5b40dadae
urt <urt5002urt@gmail.com>
2023-03-14 23:34:36 +0200

    add all but without StyleCop

---
+++ HW11_Sort/MergeSorting.cs
@@ -0,0 +1,79 @@
+﻿namespace HW15_Sort
+{
+    internal class Sorting
+    {
+        public class SortException : Exception
+        {
+            public SortException(string message) : base(message) { }
+        }
+        public static double[] MergeSort(double[] array)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+            cancellationTokenSource.CancelAfter(100);
+            var task = Task.Run(() => Sort(array, token));
+            return task.Result;
+        }
+        private static double[] Sort(double[] sortArr, CancellationToken token)
+        {
+            if (sortArr.Length == 0)
+            {
+                throw new SortException("пустой массив");
+            }
+
+            if (sortArr.Length == 1)
+            {
+                return sortArr;
+            }
+
+            var task1 = Task.Run(() => Sort(sortArr.Take(sortArr.Length / 2).ToArray(), token));
+            var task2 = Task.Run(() => Sort(sortArr.Skip(sortArr.Length / 2).ToArray(), token));
+            if (token.IsCancellationRequested)
+            {
+                throw new SortException("выполнение более 100мс");
+            }
+
+            double[] firstArr = task1.Result;
+            double[] secondArr = task2.Result;
+            int firstArrItterator = 0;
+            int secondArrItterator = 0;
+            int i = 0;
+            for (; firstArrItterator < firstArr.Length && secondArrItterator < secondArr.Length; i++)
+            {
+                if (firstArr[firstArrItterator] > secondArr[secondArrItterator])
+                {
+                    sortArr[i] = secondArr[secondArrItterator];
+                    secondArrItterator++;
+                }
+                else
+                {
+                    sortArr[i] = firstArr[firstArrItterator];
+                    firstArrItterator++;
+                }
+                if (token.IsCancellationRequested)
+                {
+                    throw new SortException("выполнение более 100мс");
+                }
+            }
+            for (; firstArrItterator < firstArr.Length; i++)
+            {
+                sortArr[i] = firstArr[firstArrItterator];
+                firstArrItterator++;
+                if (token.IsCancellationRequested)
+                {
+                    throw new SortException("выполнение более 100мс");
+                }
+            }
+            for (; secondArrItterator < secondArr.Length; i++)
+            {
+                sortArr[i] = secondArr[secondArrItterator];
+                secondArrItterator++;
+                if (token.IsCancellationRequested)
+                {
+                    throw new SortException("выполнение более 100мс");
+                }
+            }
+            return sortArr;
+        }
+    }
+}
---
+++ HW11_Sort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW15_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите коллество элементов массива");
+            int len = int.Parse(Console.ReadLine());
+            double[] array = new double[len];
+            Console.WriteLine("авто заполнение массива (0/1)");
+            if (int.Parse(Console.ReadLine()) == 1)
+            {
+                Console.WriteLine("введите максимальное значение");
+                int max = int.Parse(Console.ReadLine());
+                Random random = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next() % max;
+                }
+                if (len < 50)
+                {
+                    Console.WriteLine("исходный массив:");
+                    foreach (var item in array)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    array[i] = double.Parse(Console.ReadLine());
+                }
+            }
+            double[] sortedArr = { };
+            try
+            {
+                sortedArr = Sorting.MergeSort(array);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+
+            Console.WriteLine("отсортированный массив:");
+            foreach (var item in sortedArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
